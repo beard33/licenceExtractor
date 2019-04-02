192530908ddc9c894d450dc3cc4020dd02f641f4
beard33 <andreaguzzon26@gmail.com>
2019-04-02 20:56:21 +0200

    pushed script

--- productKeyExctractor.sh
+++ productKeyExctractor.sh
@@ -10,7 +10,7 @@ if ! [ -z $1 ]; then
 			echo "-f to cat license in a license.txt file"
 		;;
 		"-f")
-			sudo cat /sys/firmware/acpi/tables/MSDM | grep -aoP '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}' > license.txt
+			sudo cat /sys/firmware/acpi/tables/MSDM | grep -aoP '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}' > productKey.txt
 		;;
 		*) echo "Please select a valid flag: use -h to view help"
 	esac
