2c99566f908f261eb0bc7b4c90196ce55c99560e
beard33 <andreaguzzon26@gmail.com>
2019-04-02 20:42:32 +0200

    pushed script

---
+++ licenceExtractor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+input=$1
+
+if ! [ -z $1 ]; then
+	case $input in
+		"-h")
+			echo "USAGE $0 and optional"
+			echo "-h to print help"
+			echo "-f to cat license in a license.txt file"
+		;;
+		"-f")
+			sudo cat /sys/firmware/acpi/tables/MSDM | grep -aoP '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}' > license.txt
+		;;
+		*) echo "Please select a valid flag: use -h to view help"
+	esac
+else
+	sudo cat /sys/firmware/acpi/tables/MSDM | grep -aoP '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}' 
+fi
+
+
+		
