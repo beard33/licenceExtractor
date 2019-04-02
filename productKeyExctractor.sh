731b5fee7c80a87e80fe2b720574ed60b3b11395
beard33 <andreaguzzon26@gmail.com>
2019-04-02 22:52:46 +0200

    Added root check & fixed variables

--- productKeyExctractor.sh
+++ productKeyExctractor.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 input=$1
+cat /sys/firmware/acpi/tables/MSDM > tmp.txt
+key=`grep -aoP '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}' ./tmp.txt`
+rm ./tmp.txt
+
 
 if ! [ -z $1 ]; then
 	case $input in
@@ -10,12 +19,12 @@ if ! [ -z $1 ]; then
 			echo "-f to cat license in a license.txt file"
 		;;
 		"-f")
-			sudo cat /sys/firmware/acpi/tables/MSDM | grep -aoP '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}' > productKey.txt
+			echo $key > productKey.txt
 		;;
 		*) echo "Please select a valid flag: use -h to view help"
 	esac
 else
-	sudo cat /sys/firmware/acpi/tables/MSDM | grep -aoP '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}' 
+	echo $key
 fi
 
 
