e04ef425b22679cf9afc34f060cce1dcb6811a70
beard33 <andreaguzzon26@gmail.com>
2019-04-03 09:08:31 +0200

    Fixed typos

--- productKeyExctractor.sh
+++ productKeyExctractor.sh
@@ -14,9 +14,11 @@ rm ./tmp.txt
 if ! [ -z $1 ]; then
 	case $input in
 		"-h")
-			echo "USAGE $0 and optional"
-			echo "-h to print help"
-			echo "-f to cat license in a license.txt file"
+			echo""
+			echo "USAGE  $0 and optional"
+			echo""
+			echo "       -h to print help"
+			echo "       -f to cat license in a productKey.txt file"
 		;;
 		"-f")
 			echo $key > productKey.txt
